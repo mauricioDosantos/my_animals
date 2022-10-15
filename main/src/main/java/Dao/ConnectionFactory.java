a3f183617a1716c26ae266b8af4532648e292f46
Mauricio Costa <mauricio.costa@sensedata.com.br>
2022-10-15 18:26:48 -0300

    initial commit

---
+++ main/src/main/java/Dao/ConnectionFactory.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+import java.sql.*;
+
+/**
+ *
+ * @author mauricio.costa
+ */
+public class ConnectionFactory {
+    
+}
---
+++ main/src/main/java/Model/UserModel.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author mauricio.costa
+ */
+public class UserModel {
+    private int identifier;
+    private String name;
+    private String password;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+}
